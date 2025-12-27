3d83c328b1e587290446bc8069f796cba59232e9
bluenet0011-star <bluenet0011@gmail.com>
2025-12-28 01:03:00 +0900

    Fix auth type error

--- lib/auth.ts
+++ lib/auth.ts
@@ -43,8 +43,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         },
         async session({ session, token }) {
             if (token && session.user) {
-                session.user.role = token.role;
-                session.user.id = token.id;
+                session.user.role = token.role as Role;
+                session.user.id = token.id as string;
             }
             return session;
         }
