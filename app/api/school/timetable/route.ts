f145acd731691274b1c4797009b8332b621f27e8
이상민 <bluenet0011@gmial.com>
2025-12-31 16:43:34 +0900

    Fix: Change comcigan-parser import to default import

--- app/api/school/timetable/route.ts
+++ app/api/school/timetable/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { Timetable } from 'comcigan-parser';
+import Timetable from 'comcigan-parser';
 
 export async function GET(req: Request) {
     const { searchParams } = new URL(req.url);
